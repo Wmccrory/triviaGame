32219ce6ffc76767297bd6562221e9cb40fa62d8
Will <wmccrory@gmail.com>
2018-01-26 16:43:11 -0600

    Touching up welcome sequence. Question/Answer randomizer function written

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,57 @@
 //QUESTIONS OF THE LICH//////////////////////////////////////////////
 
 //Variable Bank///////////////////////////////////////////////////////
+var gameStarted = false;
+var correctAnswer;
 
 //Question arrays//
 questionBank = [];
-questionBank[0] = ["dogs", "cats",];
-questionBank[1] = ["dolphins", "whales",];
-
+	questionBank[0] = ["Question A", "Correct Answer", "Incorrect Answer A", "Incorrect Answer B", "Incorrect Answer C"];
+	questionBank[1] = ["Question B", "Correct Answer", "Incorrect Answer A", "Incorrect Answer B", "Incorrect Answer C"];
+	questionBank[2] = ["Question C", "Correct Answer", "Incorrect Answer A", "Incorrect Answer B", "Incorrect Answer C"];
+questionPick = [];
 //soundbank//
 var startSound = new Audio("assets/sounds/start.mp3");
 var welcomeSound = new Audio("assets/sounds/welcome.mp3");
 var themeSound = new Audio("assets/sounds/theme.mp3");
 
 //Element mechanics///////////////////////////////////////////////////
+
+//Get current question//
+function questionGet() {
+	var questionNumber = Math.floor(Math.random() * questionBank.length);
+	var questionPick = questionBank[questionNumber];
+	questionBank.splice(questionNumber, 1);
+	$("#questionText").text(questionPick[0]);
+	questionPick.splice(questionPick[0], 1);
+	correctAnswer = questionPick[0];
+
+	var answerPicker = Math.floor(Math.random() * questionPick.length);
+	console.log(answerPicker);
+	$("#answeratext").text(questionPick[answerPicker]);
+	questionPick.splice(answerPicker, 1);
+
+	var answerPicker = Math.floor(Math.random() * questionPick.length);
+	console.log(answerPicker);
+	$("#answerbtext").text(questionPick[answerPicker]);
+	questionPick.splice(answerPicker, 1);
+
+	var answerPicker = Math.floor(Math.random() * questionPick.length);
+	console.log(answerPicker);
+	$("#answerctext").text(questionPick[answerPicker]);
+	questionPick.splice(answerPicker, 1);
+
+	var answerPicker = Math.floor(Math.random() * questionPick.length);
+	console.log(answerPicker);
+	$("#answerdtext").text(questionPick[answerPicker]);
+	questionPick.splice(answerPicker, 1);
+
+	console.log(correctAnswer);
+	console.log(questionNumber);
+	console.log(questionPick);
+	console.log(questionBank);
+}
+
 
 //Slide Out//
 function slideOut() {
@@ -42,25 +81,36 @@ function muteSound() {
 	startSound.muted=true;
 }
 
-//function fadeOut {
+//Welcome sequence
 function fadeWelcome() {
+	gameStarted = true;
 	$("#startGame").html("<h3>Get Ready</h3>");
 	setTimeout(function() {
 		themeSound.play();
 		$("#welcome").fadeOut(500);
 		$("#question").animate({left: '0%'}, 1000);
+		
 		setTimeout(function() {
 			$("#question").animate({left: '-100%'}, 1000);	
-		}, 2000);
+		}, 2500);
+		
 		setTimeout(function() {
-			$("#question").html("<h1>Starring Faceless Void!</h1>");
+			$("#questionText").text("Starring Faceless Void!");
+			$("#answerb").html("<h1>And You!</h1");
 			$('#quizmaster').animate({right: '0%'}, 950); 
 			$("#question").animate({left: '0%'}, 1000);
-			$("#answera").animate({left: '0%'}, 1000)
+			$("#answerb").animate({left: '0%'}, 1000)
 		}, 3500);
+		
 		setTimeout(function() {
 			slideOut()
-		}, 6000);
+		}, 6500);
+
+		setTimeout(function() {
+			$("#answerb").html("<h2 id='answerbtext'>Answer B</h2");
+			questionGet()
+			slideIn()
+		}, 7500);
 	}, 3000);
 }
 
@@ -79,7 +129,15 @@ window.onload = function() {
 	welcome()
 
 $("#startGame").on("click", function() {
+	if (!gameStarted) 
+	{
 	startSound.play();
 	fadeWelcome()
+	}
 });
 }//End of game progression
+
+//Test stuff
+$("#slideButton").on("click", function () {
+	questionGet()
+})
