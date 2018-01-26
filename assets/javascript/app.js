416a4babd19ee6e034566ba063e1ab31cc6129fa
Will <wmccrory@gmail.com>
2018-01-25 22:40:48 -0600

    Welcome sequence completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 //QUESTIONS OF THE LICH//////////////////////////////////////////////
 
+//Variable Bank///////////////////////////////////////////////////////
+
+//soundbank//
+var startSound = new Audio("assets/sounds/start.mp3");
+var welcomeSound = new Audio("assets/sounds/welcome.mp3");
+var themeSound = new Audio("assets/sounds/theme.mp3");
+
 //Element mechanics///////////////////////////////////////////////////
 
 //Slide Out//
@@ -15,20 +22,46 @@ function slideIn() {
 }
 
 //Fade In
-function unfade() {
+function welcome() {
+	welcomeSound.play();
 	$("#welcome").fadeIn(2500);
+	setTimeout(function () {
+		// themeSound.play()
+	}, 7000);
+}
+
+//Mute Button
+function muteSound() {
+	welcomeSound.muted=true;
+	themeSound.muted=true;
+	startSound.muted=true;
 }
 
 //function fadeOut {
-function fade() {	
-	$("#welcome").fadeOut(500);
+function fadeWelcome() {
+	$("#startGame").html("<h3>Get Ready</h3>");
+	setTimeout(function() {
+		themeSound.play();
+		$("#welcome").fadeOut(500);
+		$("#question").animate({left: '0%'}, 1000);
+		setTimeout(function() {
+			$("#question").animate({left: '-100%'}, 1000);	
+		}, 2000);
+		setTimeout(function() {
+			$("#question").html("<h1>Starring Faceless Void!</h1>");
+			$('#quizmaster').animate({right: '0%'}, 950); 
+			$("#question").animate({left: '0%'}, 1000);
+			$("#answera").animate({left: '0%'}, 1000)
+		}, 3500);
+		setTimeout(function() {
+			slideOut()
+		}, 6000);
+	}, 3000);
 }
 
 //test button//
-
-$("#button").on("click", function() {
-	unfade()
-	slideOut()
+$("#muteButton").on("click", function() {
+	muteSound()
 });
 
 $("#buttonIn").on("click", function() {
@@ -36,6 +69,12 @@ $("#buttonIn").on("click", function() {
 	slideIn()
 });
 
+//Game progression////////////////////////////////////////////////
 window.onload = function() {
-	unfade()
-}
+	welcome()
+
+$("#startGame").on("click", function() {
+	startSound.play();
+	fadeWelcome()
+});
+}//End of game progression
